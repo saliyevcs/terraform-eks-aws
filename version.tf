06be6d7a6cc6f927784a47a348ecc401a01f4c74
saliyevcs <saliyevcs@gmail.com>
2023-11-01 12:17:24 -0400

    version update

--- version.tf
+++ version.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_version = "~> 1.5.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
